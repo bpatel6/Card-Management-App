e3b1b83a0d9576a4660fa4abd11c68242303dec7
Bhavik <bpatel6@uiowa.edu>
2020-11-08 18:04:18 -0600

    added extra card to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   cards = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 cards=[{:card_value=>'1',:card_suit=>'Spades',:pile_id=>0},
-              {:card_value=>'2',:card_suit=>'Spades',:pile_id=>0}]
+       {:card_value=>'2',:card_suit=>'Spades',:pile_id=>0},
+       {:card_value=>'3',:card_suit=>'Hearts',:pile_id=>0},
+       {:card_value=>'K',:card_suit=>'Diamonds',:pile_id=>0},
+       {:card_value=>'Q',:card_suit=>'Clubs',:pile_id=>0},
+       {:card_value=>'J',:card_suit=>'Hearts',:pile_id=>0},
+       {:card_value=>'6',:card_suit=>'Diamonds',:pile_id=>0}]
 
 cards.each do |card|
   Card.create!(card)
