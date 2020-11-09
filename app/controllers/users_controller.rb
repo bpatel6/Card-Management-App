a33c253d5a17eaa4d18b47cc0cebf99ba44b7046
Bhavik <bpatel6@uiowa.edu>
2020-11-08 18:03:53 -0600

    added method to draw a card from a pile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+
   def show
-    @hand_cards = Card.all
+    @hand_cards = Card.where(pile_id: 666)
   end
+
+  def draw
+    cards = Card.where(pile_id: 0)
+    card = cards[rand(cards.length)]
+    if card.nil?
+      flash[:notice] = 'No card available in the deck'
+    else
+      card.update(pile_id: 666)
+      flash[:notice] = "You got #{card.card_value} of #{card.card_suit}"
+    end
+    redirect_to users_show_path
+  end
+
 end
