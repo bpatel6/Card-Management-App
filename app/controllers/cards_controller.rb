c39b8e124760a8b7edc76f10ed97cc25d8eb2865
Nick Smith <nicholas-m-smith@uiowa.edu>
2020-11-16 12:01:27 -0600

    Added code to deal out the number of requested cards to the current dummy user, along with scaffolding to extend the multi-deal to multiple users.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,6 +6,22 @@ class CardsController < ApplicationController
   end
 
   def deal_all
+    puts(params[:num_cards])
+    deck = Card.where(pile_id: 0)
+    cards = []
+    # users = User.# get user IDs
+    user = 666
+    dealnum = Integer(params[:num_cards])
+
+    # users.each do |user|
+      i = 0
+      while i < dealnum do
+        card = deck[rand(deck.length)]
+        card.update(pile_id: user)
+        i=i+1
+      end
+    # end
+    flash[:notice] = "You dealt #{user.length*:num_cards} cards"
     redirect_to cards_path
   end
 
