382e2dfddaea7510b162854742e670cb72f3f724
Bhavik <bpatel6@uiowa.edu>
2020-11-30 14:42:38 -0600

    schema modified to provide user with an account id

--- db/schema.rb
+++ db/schema.rb
@@ -9,19 +9,14 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2020_11_15_221900) do
+
+ActiveRecord::Schema.define(version: 2020_11_21_033613) do
 
   create_table "cards", force: :cascade do |t|
     t.string "card_value"
     t.string "card_suit"
     t.integer "pile_id"
   end
-  
-  create_table "settings", force: :cascade do |t|
-    t.string "uid"
-    t.integer "num_decks"
-    t.string "num_players"
-    t.string "deck_settings"
 
   create_table "scores", force: :cascade do |t|
     t.string "name"
@@ -29,10 +24,19 @@ ActiveRecord::Schema.define(version: 2020_11_15_221900) do
     t.integer "score"
   end
 
+  create_table "settings", force: :cascade do |t|
+    t.string "uid"
+    t.integer "num_decks"
+    t.string "num_players"
+    t.string "deck_settings"
+    t.string "session_token"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "provider"
     t.string "uid"
+    t.integer "account_id"
     t.string "email"
     t.string "encrypted_password"
     t.string "session_token"
