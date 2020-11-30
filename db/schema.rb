b2ad58bcbd12cbf8984e34dce3260a0220afa31c
mattperng00 <mattperng@gmail.com>
2020-11-30 12:31:03 -0600

    added end in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_15_221900) do
     t.integer "num_decks"
     t.string "num_players"
     t.string "deck_settings"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
