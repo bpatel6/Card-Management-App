4614ee771abc50a9f3326129fe3887502cc29f4c
Bhavik <bpatel6@uiowa.edu>
2020-12-04 17:18:44 -0600

    configured the routes for create session and join session

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,21 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'sessions/show'
+  post 'sessions/createSession'
+  post 'sessions/joinSession'
 
   resources :cards
   root to: redirect('/cards')
 
   resources :users
   get 'users/show'
+  get :user_hand, controller: :users
   post 'users/draw'
   post 'cards/deal_all'
   post 'users/update'
   post 'users/increment_score'
   post 'users/decrement_score'
-
 
   resources :settings
   root :to => redirect('/settings')
