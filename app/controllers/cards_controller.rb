1ae7bf0a85f76a5e5f3c62be3463c8b2da881809
Nick Smith <nicholas-m-smith@uiowa.edu>
2020-12-09 04:10:13 -0600

    Deal_all seemingly works looking at DB files but doesn't work from UI

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,12 +6,15 @@ class CardsController < ApplicationController
   end
 
   def deal_all
-    # users = User.# get user IDs
-    user = 666
+    users = User.pluck(:account_id)
+    users.each do |user|
+      puts(user)
+    end
+
     dealnum = Integer(params[:num_cards])
 
     # loops through each user
-    # users.each do |user|
+    users.each do |user|
 
       # loops through the requested number of cards
       i = 0
@@ -19,16 +22,21 @@ class CardsController < ApplicationController
         # gets the most updated form of the deck
         deck = Card.where(pile_id: 0)
         # gets a random card
+        if(deck.length==0)
+          flash[:notice]= "The Deck is Out of Cards"
+        end
         card = deck[rand(deck.length)]
         # puts that random card in the current user's hand
         card.update(pile_id: user)
         # increments loop counter
         i=i+1
       end
-    # end
-    flash[:notice] = "You dealt #{dealnum} cards"
-    # flash[:notice] = "You dealt #{users.length*dealnum} cards"
-    redirect_to cards_path
+      # end
+      flash[:notice] = "You dealt #{users.length*dealnum} cards"
+    end
+  redirect_to users_show_path
+
   end
+
 
 end
