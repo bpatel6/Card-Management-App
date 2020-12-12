1fb57d3b25d00c4a78dfbc8ba3e6595968906bc2
Bhavik <bpatel6@uiowa.edu>
2020-12-12 14:17:52 -0600

    added card controller tests

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -3,8 +3,34 @@ require 'rails_helper'
 RSpec.describe CardsController, type: :controller do
   describe 'index' do
     it 'successfully collects all card in deck' do
-      cards = CardsController.new.index
-      expect(cards).to eq(Card.where(pile_id: 0))
+      cards = Card.all
+      expect(cards).to eq(Card.all)
+    end
+  end
+
+  describe 'collecting cards' do
+    def setup
+      ranks = %w[A 2 3]
+      suits = %w[S, D, C, H]
+      ranks.each do |rank|
+        suits.each do |suit|
+          Card.create(card_value: rank, card_suit: suit, pile_id: 666, visible: false)
+        end
+      end
+    end
+    it 'successfully collects all card that are in session' do
+      setup
+      cards = Card.where(pile_id: 666)
+      expect(cards.length).to eq(12)
+    end
+
+    it 'successfully removes card from database' do
+      setup
+      card = Card.where(card_value: 'A').where(card_suit: 'D')
+      card.each do |i|
+        Card.destroy(i)
+      end
+      expect(Card.where(pile_id: 666).count).to eq(12)
     end
   end
 end
