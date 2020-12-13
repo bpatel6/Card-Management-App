83352d29b682e6cdf221e275dafaf914417e8012
Nick Smith <nicholas-m-smith@uiowa.edu>
2020-12-12 20:06:08 -0600

    merged main in locally and re-committing working deal all tests

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -3,34 +3,8 @@ require 'rails_helper'
 RSpec.describe CardsController, type: :controller do
   describe 'index' do
     it 'successfully collects all card in deck' do
-      cards = Card.all
-      expect(cards).to eq(Card.all)
-    end
-  end
-
-  describe 'collecting cards' do
-    def setup
-      ranks = %w[A 2 3]
-      suits = %w[S, D, C, H]
-      ranks.each do |rank|
-        suits.each do |suit|
-          Card.create(card_value: rank, card_suit: suit, pile_id: 666, visible: false)
-        end
-      end
-    end
-    it 'successfully collects all card that are in session' do
-      setup
-      cards = Card.where(pile_id: 666)
-      expect(cards.length).to eq(12)
-    end
-
-    it 'successfully removes card from database' do
-      setup
-      card = Card.where(card_value: 'A').where(card_suit: 'D')
-      card.each do |i|
-        Card.destroy(i)
-      end
-      expect(Card.where(pile_id: 666).count).to eq(12)
+      cards = CardsController.new.index
+      expect(cards).to eq(Card.where(pile_id: 0))
     end
   end
 
@@ -68,7 +42,7 @@ RSpec.describe CardsController, type: :controller do
       controller.deal_all
 
 
-    # verify that each user has 3 cards
+      # verify that each user has 3 cards
       expect(Card.where(pile_id: 123).count).to eq(3)
       expect(Card.where(pile_id: 231).count).to eq(3)
       expect(Card.where(pile_id: 321).count).to eq(3)
