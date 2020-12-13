05ee9efb46b9f108d7796055ecd93223c888305e
bajacobson <bryce-jacobson@uiowa.edu>
2020-12-12 21:17:08 -0600

    Adding tests for the user controllers and the card controllers

--- test/controllers/cards_controller_test.rb
+++ test/controllers/cards_controller_test.rb
@@ -6,4 +6,12 @@ class CardsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  describe CardsController do
+    describe 'dealing to all players' do
+      cards = Card.where(pile_id: current_user.active_session)
+      CardsController.deal_all
+      assert(cards-52 == Card.where(pile_id: current_user.active_session))
+    end
+  end
+
 end
