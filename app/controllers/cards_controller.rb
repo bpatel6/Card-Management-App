7605471091dbf8582df9d3a79bf3591ce10b8d19
Bhavik <bpatel6@uiowa.edu>
2020-12-12 19:22:05 -0600

    fixed a deal_all error with flash message

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,33 +11,36 @@ class CardsController < ApplicationController
 
   def deal_all
     users = User.where(active_session: current_user.active_session)
-    deck = Card.where(pile_id: current_user.active_session)
     dealnum = Integer(params[:num_cards])
-
+    deck = Card.where(pile_id: current_user.active_session)
     if deck.length == 0
-      flash[:notice] = "The deck ran out of cards"
-      redirect_to users_show_path
+      redirect_to users_show_path, notice: 'No cards available to deal in deck'
     else
+      # loops through each user
       users.each do |user|
-        deck = Card.where(pile_id: current_user.active_session)
         # loops through the requested number of cards
-        if deck.length == 0
-          break
-        end
         i = 0
         while i < dealnum do
+          # gets the most updated form of the deck
+          deck = Card.where(pile_id: current_user.active_session)
+          # gets a random card
           if deck.length == 0
+            flash[:notice] = "The deck ran out of cards"
             break
           end
           card = deck[rand(deck.length)]
           # puts that random card in the current user's hand
           card.update(pile_id: user.account_id)
           # increments loop counter
-          i = i+1
+          i = i + 1
         end
-        flash[:notice] = "You dealt #{users.length*i} cards"
+        # end
+        flash[:notice] = "You dealt cards"
       end
       redirect_to users_show_path
     end
+
   end
+
+
 end
