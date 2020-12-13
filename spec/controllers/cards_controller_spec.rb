12714a03a51142004818f579a372d73e36774a5b
Nick Smith <nicholas-m-smith@uiowa.edu>
2020-12-12 20:01:56 -0600

    deal all tests working.

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -12,53 +12,40 @@ RSpec.describe CardsController, type: :controller do
   describe 'deal all' do
     it 'successfully deals cards to all users' do
       # given there are 3 users:
-      User.create!(:name=>'Alice', :provider=>'SELT', :uid=>'123', :account_id=>123, :email=>'alice@gmail.com', :encrypted_password=>'null', :session_token=>SecureRandom.base64, :active_session =>"1234")
-      User.create!(:name=>'Bob', :provider=>'SELT', :uid=>'231', :account_id=>231, :email=>'bob@gmail.com', :encrypted_password=>'null', :session_token=>SecureRandom.base64, :active_session =>"1234")
-      User.create!(:name=>'Craig', :provider=>'SELT', :uid=>'321', :account_id=>321, :email=>'craig@gmail.com', :encrypted_password=>'null', :session_token=>SecureRandom.base64, :active_session =>"1234")
-      @current_user = User.find_by(name: "Alice")
+      User.create!(:name=>'Alice', :provider=>'SELT', :uid=>'123', :account_id=>123, :email=>'alice@gmail.com', :encrypted_password=>'null', :session_token=>SecureRandom.base64, :active_session =>1234)
+      User.create!(:name=>'Bob', :provider=>'SELT', :uid=>'231', :account_id=>231, :email=>'bob@gmail.com', :encrypted_password=>'null', :session_token=>SecureRandom.base64, :active_session =>1234)
+      User.create!(:name=>'Craig', :provider=>'SELT', :uid=>'321', :account_id=>321, :email=>'craig@gmail.com', :encrypted_password=>'null', :session_token=>SecureRandom.base64, :active_session =>1234)
 
       # given there are at least 9 cards:
-      cards=[{:card_value=>'A',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'K',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'Q',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'J',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'10',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'9',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'8',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'7',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'6',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'5',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'4',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'3',:card_suit=>'H',:pile_id=>0, :visible=>false},
-             {:card_value=>'2',:card_suit=>'H',:pile_id=>0, :visible=>false}]
+      cards=[{:card_value=>'A',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'K',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'Q',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'J',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'10',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'9',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'8',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'7',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'6',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'5',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'4',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'3',:card_suit=>'H',:pile_id=>1234, :visible=>false},
+             {:card_value=>'2',:card_suit=>'H',:pile_id=>1234, :visible=>false}]
       cards.each do |card|
         Card.create!(card)
       end
 
       # deal 3 cards
       params = ActionController::Parameters.new(num_cards: 3)
-      # controller.deal_all
-    end
-    it 'user1 has 3 cards' do
+      allow(controller).to receive(:current_user) {User.find_by(name: 'Alice')}
+      allow(controller).to receive(:params) {params}
+      controller.should_receive(:redirect_to).and_return(true)
+      controller.deal_all
+
 
     # verify that each user has 3 cards
-      newcards = Card.where(pile_id: 123)
-      newcards.each do |card|
-        puts card
-      end
-      expect(newcards.count).to eq(0)
-    end
-
-    it 'user1 has 3 cards' do
-
-    newcards = Card.where(pile_id: 231)
-
-      expect(newcards.count).to eq(0)
-    end
-
-    it 'user1 has 3 cards' do
-    newcards = Card.where(pile_id: 321)
-      expect(newcards.count).to eq(0)
+      expect(Card.where(pile_id: 123).count).to eq(3)
+      expect(Card.where(pile_id: 231).count).to eq(3)
+      expect(Card.where(pile_id: 321).count).to eq(3)
     end
   end
 end
